9e6a6e32fe458f0e470c6802c6cb7081b69225b2
bharath-kadiyala <kadiyalabharath8877@gmail.com>
2023-04-08 12:02:54 +0000

    commit first project

---
+++ index.mjs
@@ -0,0 +1,9 @@
+import { StudentDetails, CarDetails } from "./sample.mjs";
+
+const student = new StudentDetails("Bharath", 25);
+console.log(student);
+console.log(student.name);
+
+const car = new CarDetails("Honda", "Blue");
+console.log(car);
+console.log(car.color);
---
+++ sample.mjs
@@ -0,0 +1,13 @@
+export class StudentDetails {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+export class CarDetails {
+  constructor(brand, color) {
+    this.brand = brand;
+    this.color = color;
+  }
+}
